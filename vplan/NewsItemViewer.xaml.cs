22fdd6c00937d914844832b4ed321a26d48a918b
Martin Haug <mhaug@live.de>
2014-08-07 21:37:51 +0200

    Incredible news viewer

---
+++ vplan/NewsItemViewer.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using UntisExp;
+
+namespace vplan
+{
+    public partial class NewsItemViewer : PhoneApplicationPage
+    {
+        Settings settings = new Settings();
+        News theNews;
+        public NewsItemViewer()
+        {
+            InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                theNews = (News)settings.read("selectedNews");
+                DataContext = theNews;
+            }
+            catch {
+                Uri uri = new Uri("/MainPage.xaml", UriKind.Relative);
+                (Application.Current.RootVisual as PhoneApplicationFrame).Navigate(uri);
+            }
+        }
+
+    }
+}
